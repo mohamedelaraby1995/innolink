cef1813e7b12ab2f3470affa2b07edec9d98d2c0
mohamedelaraby1995 <mohamedelaraby1995@gmail.com>
2025-12-26 22:46:59 +0200

    finishing Setup the components and layouts

--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -14,26 +14,8 @@
                             </h3>
                             <div class="text-xl font-bold mb-4">{{ $listing->company_name; }}</div>
                             <ul class="flex">
-                                <li
-                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                                >
-                                    <a href="#">Laravel</a>
-                                </li>
-                                <li
-                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                                >
-                                    <a href="#">API</a>
-                                </li>
-                                <li
-                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                                >
-                                    <a href="#">Backend</a>
-                                </li>
-                                <li
-                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                                >
-                                    <a href="#">Vue</a>
-                                </li>
+
+                               <x-listing-card-tags :tagsCsv="$listing->tags"/>
                             </ul>
                             <div class="text-lg mt-4">
                                 <i class="fa-solid fa-location-dot"></i> {{$listing->location;}}
